999a77826f6e0f42405cbde8548ecb611163a14e
Eduardo Seije <eduardoseije@gmail.com>
2021-02-24 19:16:38 -0300

    Exercício 5.1-6 JavaScript

--- exercises/Bloco5-JS/bloco5.js
+++ exercises/Bloco5-JS/bloco5.js
@@ -1,4 +1,4 @@
-// Exercício 1
+// Exercício 1 - Operações
 let a = 4;
 let b = 9;
 
@@ -17,7 +17,7 @@ console.log(div);
 let mod = (a % b);
 console.log(mod);
 
-// Exercício 2
+// Exercício 2 - Maior de 2 números
 let num1 = 9;
 let num2 = 7;
 
@@ -28,7 +28,7 @@ else {
     console.log(num2);
 }
 
-// Exercício 3
+// Exercício 3 - Maior de 3 números
 let num1 = 4683;
 let num2 = 9;
 let num3 = 500;
@@ -43,7 +43,7 @@ else {
     console.log(num3);
 }
 
-// Exercício 4
+// Exercício 4 - Positovo ou negativo?
 let number = -165146513;
 
 if (number > 0) {
@@ -56,7 +56,7 @@ else {
     console.log("Zero")
 }
 
-// Exercício 5
+// Exercício 5 - Triângulo ou não?
 let angle1 = 80;
 let angle2 = 60;
 let angle3 = 70;
@@ -67,4 +67,37 @@ console.log(sumAngles)
 }
 else {
     console.log(sumAngles)
+}
+
+// Exercício 6 - Movimentos Xadrez
+let chessPiece = "king";
+
+switch (chessPiece.toLowerCase()) {
+    case ("pawn"):
+        console.log("Forward - 1 vacant square");
+        break;
+         
+    case("rook"):
+        console.log("Forward and Backward");
+        break;
+
+    case("bishop"):
+        console.log("Diagonal");
+        break;
+
+    case("queen"):
+        console.log("Any direction");
+        break;
+
+    case("king"):
+        console.log("Any direction - 1 vacant square");
+        break;
+
+    case("knight"):
+        console.log("Diagonal from one corner of any 2×3 rectangle of squares to the furthest opposite corner.");
+        break;
+
+    default:
+        console.log("Error")
+
 }
