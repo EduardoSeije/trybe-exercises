df1198401439121ce766bc71d889308faf0957e9
Eduardo Seije <eduardoseije@gmail.com>
2021-02-24 18:21:11 -0300

    Exercísio 5.1-1-JavaScript

---
+++ exercises/Bloco5-JS/bloco5.js
@@ -0,0 +1,17 @@
+let a = 4;
+let b = 9;
+
+let soma = (a + b);
+console.log(soma);
+
+let subtracao = (a - b);
+console.log(subtracao);
+
+let mult = (a * b);
+console.log(mult);
+
+let div = (a / b); 
+console.log(div);
+
+let mod = (a % b);
+console.log(mod);
