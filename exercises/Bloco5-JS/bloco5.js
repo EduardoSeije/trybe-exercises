5c176032690045e7acaeffc4dbae7f99b15ea671
Eduardo Seije <eduardoseije@gmail.com>
2021-02-24 18:31:40 -0300

    Exercício 5.1-2 JavaScript

--- exercises/Bloco5-JS/bloco5.js
+++ exercises/Bloco5-JS/bloco5.js
@@ -1,3 +1,4 @@
+// Exercício 1
 let a = 4;
 let b = 9;
 
@@ -15,3 +16,14 @@ console.log(div);
 
 let mod = (a % b);
 console.log(mod);
+
+// Exercício 2
+let num1 = 9;
+let num2 = 7;
+
+if (num1 > num2) {
+    console.log(num1)
+}
+else {
+    console.log(num2);
+}
