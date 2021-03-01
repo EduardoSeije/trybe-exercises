15487fc7f958f6a51f3b9eb80f6a2e24f6a7219c
Eduardo Seije <eduardoseije@gmail.com>
2021-03-01 19:37:04 -0300

    Exercício 4.4-2b JavaScript

--- bloco_4/exercicios4.4.b.js
+++ bloco_4/exercicios4.4.b.js
@@ -11,3 +11,16 @@ function verificarPalindrome(word) {
     }
 } 
 verificarPalindrome(word);    
+
+// Exercício 02 - Crie uma função que receba um array de inteiros e retorne o maior
+let array = [2, 3, 6, 7, 10 , 152]
+let biggestNumber = 0;
+function biggestNumbers (array) {
+    for (let index = 0; index <= array.length; index ++) {
+        if (array[index] > biggestNumber) {
+            biggestNumber = array[index];
+        }
+    }
+    console.log(biggestNumber);
+}
+biggestNumbers(array);
