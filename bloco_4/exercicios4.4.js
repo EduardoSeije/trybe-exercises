4590aab35ad736ea6e94b2c3e61c9bb1a4b941b5
Eduardo Seije <eduardoseije@gmail.com>
2021-03-01 18:39:22 -0300

    Exercício 4.4-1 JavaScript

---
+++ bloco_4/exercicios4.4.js
@@ -0,0 +1,8 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+// Exercício 01 - imprima uma mensagem de boas vindas para a personagem .
+console.log('Bem-vinda, ' + info['personagem']);
